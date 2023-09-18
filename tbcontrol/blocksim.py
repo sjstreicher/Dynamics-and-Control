296c2b7723e3905583eb39b74431afbb6e7ee349
simon <streichersj@gmail.com>
2023-09-18 17:29:55 +0100

    Formatting

--- tbcontrol/blocksim.py
+++ tbcontrol/blocksim.py
@@ -1,7 +1,9 @@
 from collections import defaultdict
+
+import numpy
 import scipy
 import scipy.signal
-import numpy
+
 
 class Block:
     def __init__(self, name, inputname, outputname):
@@ -15,13 +17,15 @@ class Block:
 
 class LTI(Block):
     """Represents a general Linear Time Invariant system with optional delay"""
+
     def __init__(self, name, inputname, outputname, numerator, denominator=1, delay=0):
-        """:param name: str, The name of the block
-           :param inputname: str, the name of the input signal
-           :param outputname: str, the name of the output signal
-           :param numerator: number or iterable of numbers for numerator coefficients in descending order
-           :param denominator:  number or iterable of numbers for denominator coefficients in descending order
-           :param delay: number, delay
+        """
+        :param name: str, The name of the block
+        :param inputname: str, the name of the input signal
+        :param outputname: str, the name of the output signal
+        :param numerator: number or iterable of numbers for numerator coefficients in descending order
+        :param denominator:  number or iterable of numbers for denominator coefficients in descending order
+        :param delay: number, delay
 
         """
         super().__init__(name, inputname, outputname)
@@ -55,7 +59,16 @@ class LTI(Block):
 
 
 class Controller(LTI):
-    def __init__(self, name, inputname, outputname, numerator, denominator=1, delay=0, automatic=True):
+    def __init__(
+        self,
+        name,
+        inputname,
+        outputname,
+        numerator,
+        denominator=1,
+        delay=0,
+        automatic=True,
+    ):
         self.automatic = True
         super().__init__(name, inputname, outputname, numerator, denominator, delay)
 
@@ -65,10 +78,11 @@ class Controller(LTI):
         else:
             return self.output
 
+
 class PI(Controller):
     def __init__(self, name, inputname, outputname, Kc, tau_i):
         """Textbook PI controller"""
-        super().__init__(name, inputname, outputname, [Kc*tau_i, Kc], [tau_i, 0])
+        super().__init__(name, inputname, outputname, [Kc * tau_i, Kc], [tau_i, 0])
 
 
 class PID(Controller):
@@ -80,13 +94,17 @@ class PID(Controller):
         if tau_d == 0:
             return PI(name, inputname, outputname, Kc, tau_i)
 
-        super().__init__(name, inputname, outputname,
-                         numerator=[Kc*alpha_f*tau_d*tau_i + Kc*tau_d*tau_i,
-                                    Kc*alpha_f*tau_d + Kc*tau_i,
-                                    Kc],
-                         denominator=[alpha_f*tau_d*tau_i,
-                                      tau_i,
-                                      0.0])
+        super().__init__(
+            name,
+            inputname,
+            outputname,
+            numerator=[
+                Kc * alpha_f * tau_d * tau_i + Kc * tau_d * tau_i,
+                Kc * alpha_f * tau_d + Kc * tau_i,
+                Kc,
+            ],
+            denominator=[alpha_f * tau_d * tau_i, tau_i, 0.0],
+        )
 
 
 class Zero(Block):
@@ -131,7 +149,7 @@ class AlgebraicEquation(Block):
     def derivative(self, e):
         return 0
 
-    
+
 class Deadtime(Block):
     def __init__(self, name, inputname, outputname, delay):
         super().__init__(name, inputname, outputname)
@@ -161,21 +179,20 @@ class Deadtime(Block):
     def derivative(self, e):
         return 0
 
-    
+
 class DiscreteTF(Block):
-    
     def __init__(self, name, input_name, output_name, dt, numerator, denominator):
         """
         Represents a discrete transfer function.
-        
-        The TF must be of the form: 
-        
+
+        The TF must be of the form:
+
                   -1       -2            -n
-        b  +  b  z   + b  z  + ... + b  z  
+        b  +  b  z   + b  z  + ... + b  z
          0     1        2             n
         -----------------------------------
                   -1       -2            -m
-        a  +  a  z   + a  z  + ... + a  z  
+        a  +  a  z   + a  z  + ... + a  z
          0     1        2             m
 
 
@@ -194,8 +211,10 @@ class DiscreteTF(Block):
         super().__init__(name, input_name, output_name)
 
         if denominator[0] == 0:
-            raise ValueError('The leading coefficient of the denominator cannot be zero')
-        
+            raise ValueError(
+                "The leading coefficient of the denominator cannot be zero"
+            )
+
         self.dt = dt
         self.y_cos = denominator[::-1]
         self.u_cos = numerator[::-1]
@@ -213,30 +232,31 @@ class DiscreteTF(Block):
         self.next_sample = 0
         self.state = 0.0
         self.output = 0.0
-    
+
     def change_input(self, t, u):
         if t > self.next_sample:
             self.next_sample += self.dt
-            
+
             self.us[:-1] = self.us[1:]
             self.us[-1] = u
-            
+
             self.ys[:-1] = self.ys[1:]
-            self.ys[-1] = None # done to ensure that if anything should go wrong, it does  
-            
+            self.ys[
+                -1
+            ] = None  # done to ensure that if anything should go wrong, it does
+
             u_sum = numpy.inner(self.u_cos, self.us)
             y_sum = numpy.inner(self.y_cos[:-1], self.ys[:-1])
-            y = (u_sum - y_sum)/self.y_cos[-1]
+            y = (u_sum - y_sum) / self.y_cos[-1]
 
             self.output = self.ys[-1] = y
         return self.output
-    
+
     def change_state(self, x):
         return 0
-    
+
     def derivative(self, e):
         return 0
-    
 
 
 class Diagram:
@@ -246,22 +266,24 @@ class Diagram:
         :param blocks: list of blocks
         :param sums: sums specified as dictionaries with keys equal to output signal and values as tuples of strings of the form "<sign><signal>"
         :param inputs: inputs specified as dictionaries with keys equal to signal names and values functions of time
-        
-        
+
+
         Example
-        
+
         >>> blocks = [Gc, G]
         >>> sums = {'e': ('+ysp', '-y')}
         >>> inputs = {'ysp': step()}
         >>> diagram = Diagram(blocks, sums, inputs)
-        
+
         """
         if not all(isinstance(block, Block) for block in blocks):
             raise TypeError("blocks must be a list of blocks")
         for output, cinputs in sums.items():
             for s in cinputs:
-                if s[0] not in '+-':
-                    raise ValueError(f"In the sum '{output}': {cinputs}, there is no sign for '{s}'")
+                if s[0] not in "+-":
+                    raise ValueError(
+                        f"In the sum '{output}': {cinputs}, there is no sign for '{s}'"
+                    )
 
         self.blocks = blocks
         self.sums = sums
@@ -282,12 +304,12 @@ class Diagram:
             signals[signal] = function(t)
         # Evaluate sums
         for output, inputs in self.sums.items():
-            signals[output] = sum(int(s[0]+'1')*signals[s[1:]] for s in inputs)
+            signals[output] = sum(int(s[0] + "1") * signals[s[1:]] for s in inputs)
         # Evaluate blocks and integrate
         for block in self.blocks:
             u = signals[block.inputname]
             signals[block.outputname] = block.change_input(t, u)
-            block.change_state(block.state + block.derivative(u)*dt)
+            block.change_state(block.state + block.derivative(u) * dt)
         return signals
 
     def simulate(self, ts, progress=False):
@@ -301,6 +323,7 @@ class Diagram:
 
         if progress:
             from tqdm.auto import tqdm as tqdm
+
             pbar = tqdm(total=len(ts))
         dt = ts[1]
         outputs = defaultdict(list)
@@ -314,17 +337,19 @@ class Diagram:
         return outputs
 
     def __repr__(self):
-        return '\n'.join(str(b) for b in self.blocks)
+        return "\n".join(str(b) for b in self.blocks)
 
 
 # Input functions
 def step(initial=0, starttime=0, size=1):
     """Return a function which can be used to simulate a step"""
+
     def stepfunction(t):
         if t < starttime:
             return initial
         else:
             return initial + size
+
     return stepfunction
 
 
@@ -335,49 +360,47 @@ def zero(t):
 
 def simple_control_diagram(Gc, G, Gd=None, Gm=None, ysp=step(), d=zero):
     """Construct a simple control diagram for quick controller simulations
-    
-                                  | d
-                               ┌─────┐
-                               │ Gd  │
-                               └──┬──┘
-                                  │ yd
- ysp +  e ┌──────┐  u  ┌─────┐ yu v     y
-    ──>o─>│  Gc  ├────>│  G  ├───>o─┬──>
-      ─↑  └──────┘     └─────┘      │
-       │                            │
-       │  ym     ┌─────┐            │
-       └─────────┤ Gm  │<───────────┘
-                 └─────┘
 
-    
-    Required arguments:
-    
-    Gc: Controller, a blocksim.Block with name='Gc', input='e', output='u' 
-    G: System, a blocksim.Block with name='G', input='u', output='yu'
-    
-    Optional arguments:
-    Gd: disturbance response, a blocksim.Block with name='Gd', input='d', output='yd'
-    ysp: a function of time to represent the input
-    d: a function of time to represent the disturbance
-    
-    Returns
-    
-    blocksim.Diagram object representing this diagram
-    
+                                     | d
+                                  ┌─────┐
+                                  │ Gd  │
+                                  └──┬──┘
+                                     │ yd
+    ysp +  e ┌──────┐  u  ┌─────┐ yu v     y
+       ──>o─>│  Gc  ├────>│  G  ├───>o─┬──>
+         ─↑  └──────┘     └─────┘      │
+          │                            │
+          │  ym     ┌─────┐            │
+          └─────────┤ Gm  │<───────────┘
+                    └─────┘
+
+
+       Required arguments:
+
+       Gc: Controller, a blocksim.Block with name='Gc', input='e', output='u'
+       G: System, a blocksim.Block with name='G', input='u', output='yu'
+
+       Optional arguments:
+       Gd: disturbance response, a blocksim.Block with name='Gd', input='d', output='yd'
+       ysp: a function of time to represent the input
+       d: a function of time to represent the disturbance
+
+       Returns
+
+       blocksim.Diagram object representing this diagram
+
     """
-    
+
     if Gd is None:
-        Gd = Zero('Gd', 'd', 'yd')
-    
+        Gd = Zero("Gd", "d", "yd")
+
     if Gm is None:
-        Gm = LTI('Gm', 'y', 'ym', 1)
-    
+        Gm = LTI("Gm", "y", "ym", 1)
+
     blocks = [Gc, G, Gd, Gm]
-    
-    sums = {'e': ('+ysp', '-ym'),
-            'y': ('+yu', '+yd')}
-    
-    inputs = {'ysp': ysp,
-              'd': d}
-    
+
+    sums = {"e": ("+ysp", "-ym"), "y": ("+yu", "+yd")}
+
+    inputs = {"ysp": ysp, "d": d}
+
     return Diagram(blocks, sums, inputs)
